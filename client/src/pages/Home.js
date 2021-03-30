8eb714e694f73a745f67c318481899e4babeb92d
Ayoubhm1993 <Ayoubhm1993@gmail.com>
2021-03-30 15:16:18 -0500

    display other pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ThoughtList from '../components/ThoughtList';
-
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_THOUGHTS } from '../utils/queries';
+
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_THOUGHTS);
